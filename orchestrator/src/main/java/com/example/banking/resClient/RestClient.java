91add952afdc4eaa0a9804f90d3cdcde09baf32a
sochetanie <allagorik2901@gmail.com>
2020-02-21 16:10:30 -0600

    Implemented around 80%

--- orchestrator/src/main/java/com/example/banking/resClient/RestClient.java
+++ orchestrator/src/main/java/com/example/banking/resClient/RestClient.java
@@ -1,30 +1,36 @@
 package com.example.banking.resClient;
 
+import com.google.gson.Gson;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 
 @Component
 public class RestClient {
     RestTemplate restTemplate;
-    private String statusMsg = "{\n\tNo accounts available to show currently\n}";
+    private String statusMsg = "No accounts available to show currently";
+    Gson g = new Gson();
     public RestClient(RestTemplate restTemplate) {
         this.restTemplate = restTemplate;
     }
     public String getLoanSummary(String clientId) {
         final String url = "http://localhost:9091/autoloan/getLoansByClientId/"+clientId;
         String result = restTemplate.getForObject(url, String.class);
-        return result.equals("[ ]")  ? statusMsg : result;
+        Gson g = new Gson();
+        String res = g.toJson(statusMsg);
+        return result.equals("[ ]")  ? res : result;
     }
     public String getCreditCardSummary(String clientId) {
         final String url = "http://localhost:9092/creditcard/getCreditCardsByClientId/"+clientId;
         String result = restTemplate.getForObject(url, String.class);
-        return result.equals("[ ]")  ? statusMsg : result;
+        String res = g.toJson(statusMsg);
+        return result.equals("[ ]")  ? res : result;
     }
     public String getAccDepositSummary(String clientId) {
         StringBuilder sb = new StringBuilder();
         final String url = "http://localhost:9093/deposit/getDepositAccountsByClientId/"+clientId;
         String result = restTemplate.getForObject(url, String.class);
-        System.out.println(result);
-        return result.equals("[ ]") ? statusMsg : result;
+        String res = g.toJson(statusMsg);
+        return result.equals("[ ]") ? res : result;
     }
 }
